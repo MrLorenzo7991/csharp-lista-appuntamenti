da54baed4f43d4a90ed0f66cc4a1a7d592bf72db
Lorenzo <lorenzo.miccio97@gmail.com>
2022-04-21 15:39:09 +0200

    Completato esercizio base

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento
+    {
+        private DateTime dataOrarioAppuntamento;
+        private string nomeAppuntamento;
+        private string localitaAppuntamento;
+
+        public Appuntamento(string nomeAppuntamento, string localitaAppuntamento, DateTime dataOrarioAppuntamento)
+        {
+            this.nomeAppuntamento = nomeAppuntamento;
+            this.localitaAppuntamento = localitaAppuntamento;
+            
+            if(dataOrarioAppuntamento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere nel passato");
+            }
+            else
+            {
+
+                this.dataOrarioAppuntamento = dataOrarioAppuntamento;
+            }
+        }
+
+        public void CambiaDataAppuntamento(DateTime nuovaData)
+        {
+            if (nuovaData < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere nel passato");
+            }
+            else
+            {
+
+                this.dataOrarioAppuntamento = nuovaData;
+            }
+        }
+
+
+
+        //getter e setter
+        public string GetNomeAppuntamento()
+        {
+            return nomeAppuntamento;
+        }
+        public string GetLocalitaAppuntamento()
+        {
+            return localitaAppuntamento;
+        }
+
+        public void SetNomeAppuntamento(string nuovoNome)
+        {
+            nomeAppuntamento = nuovoNome;
+        }
+
+        public void SetLocalitaAppuntamento(string nuovaLocalita)
+        {
+            localitaAppuntamento = nuovaLocalita;
+        }
+
+        //stampa Appuntamento
+        public void ToString()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Appuntamento: " + nomeAppuntamento);
+            Console.WriteLine("Presso: " + localitaAppuntamento);
+            Console.WriteLine("Alle ore: " + dataOrarioAppuntamento);
+
+        }
+    }
+}
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using ListaAppuntamenti;
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("Benvenuto nella tua agenda!");
+Console.WriteLine("Quanti appuntamenti vuoi aggiungere?");
+int numeroDiAppuntamenti = int.Parse(Console.ReadLine());           //TODO: controllo input numerico
+
+for (int i = 0; i < numeroDiAppuntamenti; i++)
+{
+    Console.WriteLine("Appuntamento numero " + (i + 1));
+    Console.WriteLine("Aggiungi il nome del tuo appuntamento: ");
+    string nomeAppuntamento = Console.ReadLine();
+    Console.WriteLine("Aggiungi il luogo del tuo appuntamento: ");
+    string localitaAppuntamento = Console.ReadLine();
+
+    bool dataCorretta = false;
+    while (!dataCorretta)
+    {
+        DateTime dataOraAppuntamento = DateTime.Now;
+        bool formatoDataCorretto = false;
+        while (!formatoDataCorretto)
+        {
+        Console.WriteLine("Aggiungi la data e l'ora del tuo appuntamento: [gg/mm/aaaa hh:mm]");
+            try
+            {
+                dataOraAppuntamento = DateTime.Parse(Console.ReadLine());                          //aggiungere controllo data corretta
+                formatoDataCorretto=true;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Il formato della data non è corretto");
+            }
+
+        }
+
+        try
+        {
+            listaAppuntamenti.Add(new Appuntamento(nomeAppuntamento, localitaAppuntamento, dataOraAppuntamento));
+            dataCorretta = true;
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
+
+foreach(Appuntamento appuntamento in listaAppuntamenti)
+{
+    appuntamento.ToString();
+}
+
+
+
+
+
+
+
+
+
+
+
